86af6e2f0352c5aebc85b3c7a4d8d98ac551572c
ilyazooo <ilyass.oumlil@yahoo.com>
2024-12-08 02:16:20 +0100

    Adding PWA feature

---
+++ src/app/home/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold ">Home</h1>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="text-5xl font-bold ">Subworld</h1>
+    <div className="flex flex-col h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold mb-4">Subworld</h1>
+      <Link href="/home" className="text-xs bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+        Get Started
+      </Link>
     </div>
   )
 }
