e05389923016e52d8cecdea53baed4f787548906
shawzu <ilyass.oumlil@yahoo.com>
2024-12-25 17:46:22 +0100

    fix UI

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,23 +1,23 @@
 export default function manifest() {
-    return {
-      name: 'Subworld',
-      short_name: 'Subworld',
-      description: 'A decentralized messaging app',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#020202',
-      theme_color: '#020202',
-      icons: [
-        {
-          src: '/Logo-192x192.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/Logo-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-    }
+  return {
+    name: 'Subworld',
+    short_name: 'Subworld',
+    description: 'A decentralized messaging app',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#111827', 
+    theme_color: '#000000', 
+    icons: [
+      {
+        src: '/Logo-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/Logo-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
   }
+}
